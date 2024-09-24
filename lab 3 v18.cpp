8ca3d9fb46f73f77bf0bbabaaf63bb83ff33efb1
VladykaMarkiyan <144060935+VladykaMarkiyan@users.noreply.github.com>
2024-09-24 13:10:20 +0300

    Update lab 3 v18.cpp

--- lab 3 v18.cpp
+++ lab 3 v18.cpp
@@ -6,7 +6,7 @@
 std::string cleanWord(const std::string& word) {
     std::string cleanedWord;
     for (char ch : word) {
-        if (std::isalpha(ch)) { 
+        if (std::isalpha(ch)) {  
             cleanedWord += ch;
         }
     }
@@ -21,33 +21,20 @@ int main() {
     std::string longestWord;
     std::string word;
 
-    bool allWordsSame = true;
-    std::string firstWord;
     std::stringstream ss(text);
-    bool first = true;
 
     while (ss >> word) {
         std::string cleanedWord = cleanWord(word);
-
-        if (first) {
-            firstWord = cleanedWord;
-            longestWord = cleanedWord;
-            first = false;
-        } else {
-            if (cleanedWord != firstWord) {
-                allWordsSame = false;
-            }
-        }
 
         if (cleanedWord.length() > longestWord.length()) {
             longestWord = cleanedWord;
         }
     }
 
-    if (allWordsSame) {
-        std::cout << "Усі введені слова однакові." << std::endl;
-    } else {
+    if (!longestWord.empty()) {
         std::cout << "Найдовше слово: " << longestWord << std::endl;
+    } else {
+        std::cout << "Не було введено жодного слова." << std::endl;
     }
 
     return 0;
