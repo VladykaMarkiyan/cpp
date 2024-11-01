18dbb2000606fc916414e964a26be80cc5ea0b96
VladykaMarkiyan <144060935+VladykaMarkiyan@users.noreply.github.com>
2024-11-01 15:21:46 +0200

    Update lab 5_v6.cpp

--- lab 5_v6.cpp
+++ lab 5_v6.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-// Функція для зміни місцями першого та останнього рядків
 void swapFirstLastRows(vector<vector<int>>& matrix) {
     int n = matrix.size();
     for (int i = 0; i < n; i++) {
@@ -11,7 +10,6 @@ void swapFirstLastRows(vector<vector<int>>& matrix) {
     }
 }
 
-// Функція для знаходження добутку елементів головної діагоналі
 int productOfDiagonal(const vector<vector<int>>& matrix) {
     int n = matrix.size();
     int product = 1;
@@ -24,13 +22,11 @@ int productOfDiagonal(const vector<vector<int>>& matrix) {
 int main() {
     int n;
     
-    // Введення розміру матриці
     cout << "Введіть розмір квадратної матриці: ";
     cin >> n;
 
     vector<vector<int>> matrix(n, vector<int>(n));
 
-    // Введення елементів матриці
     cout << "Введіть елементи матриці:" << endl;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -38,7 +34,6 @@ int main() {
         }
     }
 
-    // Виведення матриці до змін
     cout << "Початкова матриця:" << endl;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -47,10 +42,8 @@ int main() {
         cout << endl;
     }
 
-    // Зміна місцями першого і останнього рядків
     swapFirstLastRows(matrix);
 
-    // Виведення зміненої матриці
     cout << "Матриця після зміни першого і останнього рядків:" << endl;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -59,7 +52,6 @@ int main() {
         cout << endl;
     }
 
-    // Обчислення добутку елементів головної діагоналі
     int product = productOfDiagonal(matrix);
     cout << "Добуток елементів головної діагоналі: " << product << endl;
 
